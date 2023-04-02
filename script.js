99208d6e57468b05b1a71c86e5820eb20f2628b4
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-04-02 13:38:57 +0000

    [Finish] Ajax-Lesson

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const API_KEY = "7ebd63e64a87c841d658bc2f565c1e18";
+
+
+$(function(){
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid="
+                    + "7ebd63e64a87c841d658bc2f565c1e18",
+      dataType : 'jsonp',
+    }).done(function (data){
+      // ↑通信成功時の処理
+      // ↓位置
+      $('#place').text(data.name);
+      // ↓最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // ↓最低気温
+      $('#temp_min').text(data.main.temp_min);
+      // ↓湿度
+      $('#humidity').text(data.main.humidity);
+      // ↓風速
+      $('#speed').text(data.wind.speed);
+      // ↓天気
+      $('#weather').text(data.weather[0].main);
+      // ↓天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fall(function (data) {
+      // ↑通信失敗時の処理
+      alert('通信に失敗しました。');
+    });
+  });
+});
